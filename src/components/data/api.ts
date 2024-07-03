454158bd0565f11961ca72d3c8fc7a4dc32a25ac
samkhanhduy <samkhanhduy2002@gmail.com>
2024-07-03 23:21:37 +0700

    fix bugs

--- src/components/data/api.ts
+++
@@ -1,9 +0,0 @@
-import { ITask } from "../../constant/Tasks";
-
-const baseUrl = 'http://localhost:3001';
-
-export const getAllTodos = async (): Promise<ITask[]> => {
-    const res = await fetch(`${baseUrl}/tasks`);
-    const todos = await res.json();
-    return todos;
-}
